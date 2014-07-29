827a0876b881a9ac8309edbd35fc73208939f012
Sven Pachnit <sven@bmonkeys.net>
2014-07-29 06:29:48 +0200

    Srsly?

--- handlers/_buildin/world_edit_light.rb
+++ handlers/_buildin/world_edit_light.rb
@@ -39,7 +39,7 @@ module Mcl
         zip = pram[:pos1].zip(pram[:pos2])
         xd = (zip[0].max - zip[0].min).round(0) + 1
         yd = (zip[1].max - zip[1].min).round(0) + 1
-        zd = (zip[2].max - zip[2].min).round(0)
+        zd = [(zip[2].max - zip[2].min).round(0), 1].max
         xd * yd * zd
       else
         false
