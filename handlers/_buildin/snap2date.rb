dd0162f6cd4b3921582a1dac8cb12788fb8e79cc
Sven Pachnit <sven@bmonkeys.net>
2014-08-14 05:03:07 +0200

    First snap2date draft

---
+++ handlers/_buildin/snap2date.rb
@@ -0,0 +1,107 @@
+module Mcl
+  Mcl.reloadable(:HSnap2date)
+  class HSnap2date < Handler
+    attr_reader :cron, :watched_versions
+
+    def setup
+      seed_db
+      setup_parsers
+      setup_checker
+    end
+
+    def seed_db
+      Setting.seed("snap2date", "snap2date.watched_versions", "")
+      Setting.seed("snap2date", "snap2date.cron", "false")
+      @cron = Setting.get("snap2date.cron") == "true"
+      @watched_versions = Setting.fetch("snap2date.watched_versions").split(" ")
+    end
+
+    def setup_checker
+      @tick_checked = eman.tick
+      @checker = async do
+        loop do
+          Thread.current.kill if Thread.current[:mcl_halting]
+          if @cron && (eman.tick - @tick_checked) >= 250
+            @tick_checked = eman.tick
+            # @todo CHECK STUFF
+          end
+          sleep 3
+        end
+      end
+    end
+
+    def setup_parsers
+      register_command :snap2date do |handler, player, command, target, optparse|
+        handler.acl_verify(player)
+        args = command.split(" ")[1..-1]
+
+        case args[0]
+        when "status"
+          handler.tellm(player, {text: "watched versions", color: "gold"}, {text: handler.watched_versions.join(" "), color: "reset"})
+          handler.tellm(player, {text: "watcher enabled?", color: "gold"}, {text: handler.cron.to_s, color: "reset"})
+        when "check"
+          # @todo CHECK STUFF
+        when "watch"
+          if args[1]
+            handler.watch_version args[1].downcase
+            handler.tellm(player, {text: "Watching version #{args[1].downcase}...", color: "reset"})
+          else
+            handler.tellm(player, {text: "Define a version to watch!", color: "red"})
+          end
+        when "unwatch"
+          if args[1]
+            handler.unwatch_version args[1].downcase
+            handler.tellm(player, {text: "Stop watching version #{args[1].downcase}...", color: "reset"})
+          else
+            handler.tellm(player, {text: "Define a version to unwatch!", color: "red"})
+          end
+        when "update"
+          # @todo UPDATE
+        when "cron"
+          Setting.set("snap2date.cron", "true")
+          @cron = true
+          handler.tellm(player, {text: "Watcher enabled", color: "reset"})
+        when "uncron"
+          Setting.set("snap2date.cron", "false")
+          @cron = false
+          handler.tellm(player, {text: "Watcher disabled", color: "reset"})
+        else
+          handler.tellm(player, {text: "status", color: "gold"}, {text: " list watched versions and watch status", color: "reset"})
+          handler.tellm(player, {text: "check [ver]", color: "gold"}, {text: " check for [ver] or all watched versions", color: "reset"})
+          handler.tellm(player, {text: "watch <ver>", color: "gold"}, {text: " start watching <ver>", color: "reset"})
+          handler.tellm(player, {text: "unwatch <ver>", color: "gold"}, {text: " stop watching <ver>", color: "reset"})
+          handler.tellm(player, {text: "update <ver>", color: "gold"}, {text: " update to <ver>", color: "reset"})
+          handler.tellm(player, {text: "cron", color: "gold"}, {text: " check versions all 250 ticks", color: "reset"})
+          handler.tellm(player, {text: "uncron", color: "gold"}, {text: " stop checking versions all 250 ticks", color: "reset"})
+        end
+      end
+    end
+
+    def watch_version ver
+      @watched_versions = @watched_versions + [ver.downcase]
+      Setting.set("snap2date.watched_versions", @watched_versions.join(" "))
+    end
+
+    def unwatch_version ver
+      @watched_versions = @watched_versions - [ver.downcase]
+      Setting.set("snap2date.watched_versions", @watched_versions.join(" "))
+    end
+
+    def title
+      {text: "[S2D] ", color: "light_purple"}.to_json
+    end
+
+    def spacer
+      {text: " / ", color: "reset"}.to_json
+    end
+
+    def tellm p, *msg
+      $mcl.server.invoke %{/tellraw #{p} [#{title},#{msg.map(&:to_json).join(",")}]}
+    end
+
+
+    def gm mode, target
+      $mcl.server.invoke "/gamemode #{mode} #{target}"
+    end
+  end
+end
