5eb33d08c1e529270b477b1a71f28d35a97bdb6e
Sven Pachnit <sven@bmonkeys.net>
2014-08-17 00:40:56 +0200

    Fixed replace again

--- handlers/_buildin/world_edit_light.rb
+++ handlers/_buildin/world_edit_light.rb
@@ -99,8 +99,8 @@ module Mcl
         unless require_selection(p)
           is, should = a.join(" ").split(">").map(&:strip).reject(&:blank?)
           if is && should && is.is_a?(String) && should.is_a?(String)
-            is = is.split(" ")
-            $mcl.server.invoke %{/execute #{p} ~ ~ ~ fill #{pram[:pos1].join(" ")} #{pram[:pos2].join(" ")} #{is[0]} #{is[1] || "1"} replace #{should}}
+            should = should.split(" ")
+            $mcl.server.invoke %{/execute #{p} ~ ~ ~ fill #{pram[:pos1].join(" ")} #{pram[:pos2].join(" ")} #{should[0]} #{should[1] || "1"} replace #{is}}
           else
             tellm(p, {text: "!!replace <IS:TileName> [IS:dataValue] > <SHOULD:TileName> [SHOULD:dataValue]", color: "yellow"})
           end
