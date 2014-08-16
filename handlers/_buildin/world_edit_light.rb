e4e79766879e57b60b81d1940a750a8bc2ef1cf5
Sven Pachnit <sven@bmonkeys.net>
2014-08-16 12:09:33 +0200

    WEL cleanup

--- handlers/_buildin/world_edit_light.rb
+++ handlers/_buildin/world_edit_light.rb
@@ -20,49 +20,71 @@ module Mcl
       reg_pos
     end
 
-    def wel
-      {text: "[WEL] ", color: "light_purple"}.to_json
+    def title
+      {text: "[WEL] ", color: "light_purple"}
     end
 
     def spacer
-      {text: " / ", color: "reset"}.to_json
+      {text: " / ", color: "reset"}
     end
 
-    def clear_selection p
-      memory(p).delete(:pos1)
-      memory(p).delete(:pos2)
-      $mcl.server.invoke %{/tellraw #{p} [#{wel},#{{text: "Selection cleared!"}.to_json}]}
+    def tellm p, *msg
+      trawm(p, *([title] + msg))
     end
 
-    def require_pos1 p
-      pram = memory(p)
-      if pram[:pos1]
-        return false
-      else
-        $mcl.server.invoke %{/tellraw #{p} [#{wel},#{{text: "Pos1 required!"}.to_json}]}
-        return true
+
+    # ============
+    # = Commands =
+    # ============
+    def reg_sel
+      register_command "!sel [clear]", desc: "shows or clears current selection" do |h, p, c, t, a, o|
+        a[0] == "clear" ? h.clear_selection(p) : h.current_selection(p)
+      end
+
+      register_command "!set <block>", desc: "fills selection with given block" do |h, p, c, t, a, o|
+        h.acl_verify(p)
+        pram = h.memory(p)
+        unless require_selection(p)
+          $mcl.server.invoke %{/execute #{p} ~ ~ ~ fill #{pram[:pos1].join(" ")} #{pram[:pos2].join(" ")} #{a.join(" ")}}
+        end
+      end
+
+      register_command "!insert", desc: "inserts selection at given coords" do |h, p, c, t, a, o|
+        h.acl_verify(p)
+        h.insert_selection(p, a)
+      end
+
+      register_command "!stack", desc: "NotImplemented: stacks selection" do |h, p, c, t, a, o|
+        h.acl_verify(p)
+        h.stack_selection(p, a)
       end
     end
 
-    def require_pos2 p
-      pram = memory(p)
-      if pram[:pos2]
-        return false
-      else
-        $mcl.server.invoke %{/tellraw #{p} [#{wel},#{{text: "Pos2 required!"}.to_json}]}
-        return true
+    def reg_pos
+      register_command "!pos", desc: "sets pos1&2 to given coords" do |h, p, c, t, a, o|
+        h.take_pos(nil, p, a)
+      end
+
+      register_command "!spos", desc: "shifts pos1 and pos2 by given values" do |h, p, c, t, a, o|
+        h.shift_pos(nil, p, a)
+      end
+
+      [1,2].each do |num|
+        register_command "!pos#{num}", desc: "sets pos#{num} to given coords" do |h, p, c, t, a, o|
+          h.take_pos(num, p, a)
+        end
+
+        register_command "!spos#{num}", desc: "shifts pos#{num} by given values" do |h, p, c, t, a, o|
+          h.shift_pos(num, p, a)
+        end
       end
     end
 
-    def require_selection p
-      pram = memory(p)
-      if pram[:pos1] && pram[:pos2]
-        return false
-      else
-        $mcl.server.invoke %{/tellraw #{p} [#{wel},#{{text: "Selection required!"}.to_json}]}
-        return true
-      end
-    end
+
+
+    # ===========
+    # = Helpers =
+    # ===========
 
     def selection_dimensions p
       pram = memory(p)
@@ -85,25 +107,86 @@ module Mcl
       end
     end
 
+    def shift_coords one, two
+      [one[0] + two[0], one[1] + two[1], one[2] + two[2]]
+    end
+
+    def lg_coord p1, p2
+      [p1, p2]
+    end
+
+    def stack_coord_shifting lp, strdir
+      case strdir
+        when "n", "north" then [:north, :z, :-]
+        when "e", "east" then [:east, :x, :+]
+        when "s", "south" then [:south, :z, :+]
+        when "w", "west" then [:west, :x, :-]
+        when "u", "up" then [:up, :y, :+]
+        when "d", "down" then [:down, :y, :-]
+        else raise "unknown direction (n/e/s/w/u/d)"
+      end
+    end
+
+
+    # ============
+    # = Handlers =
+    # ============
+
+    def clear_selection p
+      memory(p).delete(:pos1)
+      memory(p).delete(:pos2)
+      tellm(p, {text: "Selection cleared!", color: "green"})
+    end
+
+    def require_pos1 p
+      pram = memory(p)
+      if pram[:pos1]
+        return false
+      else
+        tellm(p, {text: "Pos1 required!", color: "red"})
+        return true
+      end
+    end
+
+    def require_pos2 p
+      pram = memory(p)
+      if pram[:pos2]
+        return false
+      else
+        tellm(p, {text: "Pos2 required!", color: "red"})
+        return true
+      end
+    end
+
+    def require_selection p
+      pram = memory(p)
+      if pram[:pos1] && pram[:pos2]
+        return false
+      else
+        tellm(p, {text: "Selection required!", color: "red"})
+        return true
+      end
+    end
+
     def current_selection p, spos1 = true, spos2 = true, ssize = true
       pram = memory(p)
 
       if coords = pram[:pos1]
-        pos1 = {text: coords.join(" "), color: "aqua"}.to_json
+        pos1 = {text: coords.join(" "), color: "aqua"}
       else
-        pos1 = {text: "unset", color: "gray", italic: true}.to_json
+        pos1 = {text: "unset", color: "gray", italic: true}
       end
 
       if coords = pram[:pos2]
-        pos2 = {text: coords.join(" "), color: "blue"}.to_json
+        pos2 = {text: coords.join(" "), color: "blue"}
       else
-        pos2 = {text: "unset", color: "gray", italic: true}.to_json
+        pos2 = {text: "unset", color: "gray", italic: true}
       end
 
       if pram[:pos1] && pram[:pos2]
-        sel_size = {text: "#{selection_size(p)} blocks", color: "aqua", italic: true}.to_json
+        sel_size = {text: "#{selection_size(p)} blocks", color: "aqua", italic: true}
       else
-        sel_size = {text: "???", color: "gray", italic: true}.to_json
+        sel_size = {text: "???", color: "gray", italic: true}
       end
 
       a = []
@@ -111,8 +194,7 @@ module Mcl
       a << pos2 if spos2
       a << sel_size if ssize
 
-      cmd =  %{/tellraw #{p} [#{wel},#{a.join(",#{spacer},")}]}
-      $mcl.server.invoke(cmd)
+      tellm(p, *a.zip([",#{spacer},"] * a.count))
     end
 
     def sel_insert p, pos
@@ -120,23 +202,22 @@ module Mcl
       $mcl.server.invoke %{/execute #{p} ~ ~ ~ clone #{pram[:pos1].join(" ")} #{pram[:pos2].join(" ")} #{pos.join(" ")}}
     end
 
-    def insert_selection p, c
-      chunks = c.split(" ")[1..-1].map(&:strip).map{|i| i.to_s =~ /^-?[0-9]+$/ ? i.to_i : i}
+    def insert_selection p, a
+      chunks = a.map(&:strip).map{|i| i.to_s =~ /^-?[0-9]+$/ ? i.to_i : i}
       pram = memory(p)
 
       if chunks.count == 3
         unless require_selection(p)
           sel_insert(p, chunks)
-          $mcl.server.invoke %{/tellraw #{p} [#{wel},#{{text: "#{selection_size(p)} blocks involved"}.to_json}]}
+          tellm(p, {text: "#{selection_size(p)} blocks involved", color: "gold"})
         end
       else
-        pos = {text: "!!pos#{num} [x] [y] [z]#{" [x2] [y2] [z2]" if num.nil?}", color: "blue"}.to_json
-        $mcl.server.invoke %{/tellraw #{p} [#{h.wel},#{pos}]}
+        tellm(p, {text: "!!insert <x> <y> <z>", color: "blue"})
       end
     end
 
-    def take_pos num, p, c
-      chunks = c.split(" ")[1..-1].map(&:strip).map{|i| i.to_s =~ /^-?[0-9]+$/ ? i.to_i : i}
+    def take_pos num, p, a
+      chunks = a.map(&:strip).map{|i| i.to_s =~ /^-?[0-9]+$/ ? i.to_i : i}
       pram = memory(p)
 
       if chunks.count == 0
@@ -153,17 +234,12 @@ module Mcl
         pram[:pos2] = chunks[3..5]
         current_selection(p)
       else
-        pos = {text: "!!pos#{num} [x] [y] [z]#{" [x2] [y2] [z2]" if num.nil?}", color: "blue"}.to_json
-        $mcl.server.invoke %{/tellraw #{p} [#{h.wel},#{pos}]}
+        tellm(p, {text: "!!pos#{num} [x] [y] [z]#{" [x2] [y2] [z2]" if num.nil?}", color: "blue"})
       end
     end
 
-    def shift_coords one, two
-      [one[0] + two[0], one[1] + two[1], one[2] + two[2]]
-    end
-
-    def shift_pos num, p, c
-      chunks = c.split(" ")[1..-1].map(&:strip).map{|i| i.to_s =~ /^-?[0-9]+$/ ? i.to_i : i}
+    def shift_pos num, p, a
+      chunks = a.map(&:strip).map{|i| i.to_s =~ /^-?[0-9]+$/ ? i.to_i : i}
       pram = memory(p)
 
       if chunks.count == 0
@@ -188,34 +264,16 @@ module Mcl
           current_selection(p)
         end
       else
-        pos = {text: "!!spos#{num} [x] [y] [z]#{" [x2] [y2] [z2]" if num.nil?}", color: "blue"}.to_json
-        $mcl.server.invoke %{/tellraw #{p} [#{h.wel},#{pos}]}
+        tellm(p, {text: "!!spos#{num} [x] [y] [z]#{" [x2] [y2] [z2]" if num.nil?}", color: "blue"})
       end
     end
 
-    def lg_coord p1, p2
-      [p1, p2]
-    end
-
-    def stack_coord_shifting lp, strdir
-      case strdir
-        when "n", "north" then [:north, :z, :-]
-        when "e", "east" then [:east, :x, :+]
-        when "s", "south" then [:south, :z, :+]
-        when "w", "west" then [:west, :x, :-]
-        when "u", "up" then [:up, :y, :+]
-        when "d", "down" then [:down, :y, :-]
-        else raise "unknown direction (n/e/s/w/u/d)"
-      end
-    end
-
-    def stack_selection p, c
-      chunks = c.split(" ")[1..-1].map(&:strip).map{|i| i.to_s =~ /^-?[0-9]+$/ ? i.to_i : i}
+    def stack_selection p, a
+      chunks = a.map(&:strip).map{|i| i.to_s =~ /^-?[0-9]+$/ ? i.to_i : i}
       pram = memory(p)
 
       if chunks.count == 0
-        pos = {text: "!!stack <direction> [amount] [move_selection]", color: "blue"}.to_json
-        $mcl.server.invoke %{/tellraw #{p} [#{wel},#{pos}]}
+        tellm(p, {text: "!!stack <direction> [amount] [move_selection]", color: "blue"})
       else
         unless require_selection(p)
           direction = chunks.shift
@@ -227,65 +285,9 @@ module Mcl
           c1, c2 = p1, p2
           dirmap = stack_coord_shifting(p1, direction)
 
-          pos = {text: "#{p1.join(",")} / #{p2.join(",")} / #{dirmap[0]} (#{dirmap[2]}#{dirmap[1]})", color: "blue"}.to_json
-          $mcl.server.invoke %{/tellraw #{p} [#{wel},#{pos}]}
-        end
-      end
-    end
-
-    def reg_sel
-      register_command "!sel", desc: "shows or clears (!sel clear) current selection" do |h, p, c, t, o|
-        if c.split(" ")[1] == "clear"
-          h.clear_selection(p)
-        else
-          h.current_selection(p)
-        end
-      end
-
-      register_command "!set", desc: "fills selection with given block" do |h, p, c, t, o|
-        h.acl_verify(p)
-        pram = h.memory(p)
-        unless require_selection(p)
-          pram = memory(p)
-          $mcl.server.invoke %{/execute #{p} ~ ~ ~ fill #{pram[:pos1].join(" ")} #{pram[:pos2].join(" ")} #{c.split(" ")[1..-1].join(" ")}}
-        end
-      end
-
-      register_command "!stack", desc: "NotImplemented: stacks selection" do |h, p, c, t, o|
-        h.acl_verify(p)
-        h.stack_selection(p, c)
-      end
-
-      register_command "!insert", desc: "inserts selection at given coords" do |h, p, c, t, o|
-        h.acl_verify(p)
-        h.insert_selection(p, c)
-      end
-    end
-
-    def reg_pos
-      register_command "!pos", desc: "sets pos1 and pos2 to given coords" do |h, p, c, t, o|
-        h.take_pos(nil, p, c)
-      end
-
-      register_command "!spos", desc: "shifts pos1 and pos2 by given values" do |h, p, c, t, o|
-        h.shift_pos(nil, p, c)
-      end
-
-      [1,2].each do |num|
-        register_command "!pos#{num}", desc: "sets pos#{num} to given coords" do |h, p, c, t, o|
-          h.take_pos(num, p, c)
-        end
-
-        register_command "!spos#{num}", desc: "shifts pos#{num} by given values" do |h, p, c, t, o|
-          h.shift_pos(num, p, c)
+          tellm(p, {text: "#{p1.join(",")} / #{p2.join(",")} / #{dirmap[0]} (#{dirmap[2]}#{dirmap[1]})", color: "blue"})
         end
       end
     end
   end
 end
-
-__END__
-# mcedit light
-!!pos1   # selection start
-!!pos2   # selection end
-!!insert # insert selection here
