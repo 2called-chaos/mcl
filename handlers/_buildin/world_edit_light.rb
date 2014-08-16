034863100cc9f56fef7bda1500388c0a32c1ea69
Sven Pachnit <sven@bmonkeys.net>
2014-08-16 12:16:55 +0200

    Weird shit

--- handlers/_buildin/world_edit_light.rb
+++ handlers/_buildin/world_edit_light.rb
@@ -194,7 +194,7 @@ module Mcl
       a << pos2 if spos2
       a << sel_size if ssize
 
-      tellm(p, *a.zip([spacer] * a.length))
+      tellm(p, *a.zip([spacer] * (a.length-1)).flatten.compact))
     end
 
     def sel_insert p, pos
