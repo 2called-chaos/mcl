d464b023fab98a60f1f937dc502397874b0fd979
Sven Pachnit <sven@bmonkeys.net>
2014-08-18 02:18:07 +0200

    Fix modulo

--- handlers/_buildin/schematic_builder.rb
+++ handlers/_buildin/schematic_builder.rb
@@ -149,7 +149,7 @@ module Mcl
         deg = args[0].to_i
         if deg > 0
           if deg % 90 == 0
-            pram[:current_schematic][:rotation] = 360 % (pram[:current_schematic][:rotation] + deg)
+            pram[:current_schematic][:rotation] = (pram[:current_schematic][:rotation] + deg) % 360
             tellm(player, {text: "Schematic rotation is #{pram[:current_schematic][:rotation]} degrees", color: "yellow"})
           else
             tellm(player, {text: "Rotation must be divisible by 90", color: "red"})
