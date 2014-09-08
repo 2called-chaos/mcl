b5c4145989dcde9168a723646302bd455f2f301d
Sven Pachnit <sven@bmonkeys.net>
2014-09-08 17:31:31 +0200

    Declusterfucked main loop

--- lib/mcl/application/event_manager.rb
+++ lib/mcl/application/event_manager.rb
@@ -59,10 +59,130 @@ module Mcl
         end
       end
 
-      # that is the most awful method I've ever written, I swear!
+      module PreTick
+        def detect_mcl_lag
+          if Thread.current[:last_tick] && (Time.current - Thread.current[:last_tick]) > (app.config["tick_rate"]*2)
+            diff = Time.current - Thread.current[:last_tick]
+            app.log.warn "[CORE] main loop is lagging (delayed for #{diff.to_f - app.config["tick_rate"]}s)"
+          end
+        end
+
+        def detect_died_minecraft_server
+          if app.server.died?
+            app.log.fatal "[IPC] connection to minecraft server lost after tick ##{@tick - 1}, rebooting..."
+            raise Application::Reboot, "server connection lost after tick ##{@tick - 1}"
+          end
+        end
+
+        def detect_reboot_request
+          if $mcl_reboot
+            $mcl_reboot = false
+            raise Application::Reboot, "internal request"
+          end
+        end
+
+        def scrub_async_threads
+          if @tick % app.config["async_scrub_rate"] == 0
+            app.async.select!(&:alive?)
+          end
+        end
+
+        def scrub_promises
+          if @tick % app.config["promise_scrub_rate"] == 0
+            app.promises.select!(&:alive?)
+          end
+        end
+
+        def clear_player_cache
+          if @tick % app.config["player_cache_save_rate"] == 0
+            app.delay { app.pman.clear_cache }
+          end
+        end
+
+        def garbage_collect
+          if @tick % app.config["gc_rate"] == 0
+            GC.start
+          end
+        end
+      end
+      include PreTick
+
+      module MainTick
+        # check if promises meet condition or timed out
+        def tick_promises
+          # promises
+          app.promises.each_with_index do |promise, i|
+            begin
+              promise.tick!
+              app.promises.delete_at(i) unless promise.alive?
+            rescue Exception
+              app.handle_exception($!) do |ex|
+                app.log.error "PromiseTickError on tick #{@tick}: (#{ex.class.name}) #{ex.message}"
+              end
+            end
+          end
+        end
+
+        # call all handlers (shortticktime)
+        def tick_handlers
+          app.handlers.each do |handler|
+            begin
+              handler.tick!
+            rescue Exception
+              app.handle_exception($!) do |ex|
+                app.log.error "ShortTickPerformError on tick #{@tick}: (#{ex.class.name}) #{ex.message}"
+              end
+            end
+          end
+        end
+
+        # call delayed handlers
+        def tick_delayed
+          while cb = app.delayed.shift
+            begin
+              cb.call
+            rescue Exception
+              app.handle_exception($!) do |ex|
+                app.log.error "DelayedPerformError on tick #{@tick}: (#{ex.class.name}) #{ex.message}"
+              end
+            end
+          end
+        end
+
+        # process spool and call handlers accordingly
+        def tick_spool &block
+          while !@spool.empty?
+            val = @spool.pop(true) rescue nil
+            break if val.nil?
+            block.call(val)
+
+            begin
+              # parse event
+              evd = parser.classify(val.chomp)
+            rescue Exception
+              app.handle_exception($!) do |ex|
+                app.log.error "EventParseError on tick #{@tick}: (#{ex.class.name}) #{ex.message}"
+              end
+            end
+          end
+        end
+
+        # call scheduler which may invoke tasks which were delayed previously
+        def tick_scheduler
+          begin
+            app.scheduler.tick!
+          rescue Exception
+            app.handle_exception($!) do |ex|
+              app.log.error "SchedulerPerformError on tick #{@tick}: (#{ex.class.name}) #{ex.message}"
+            end
+          end
+        end
+      end
+      include MainTick
+
+      # main loop
       def tick!
         # raise Application::Halt, "ticklimit of 500 reached" if @tick > 500
-
         app.sync do
           begin
             @tick += 1
@@ -70,116 +190,29 @@ module Mcl
             processtime, shortticktime, delayedtime, schedulertime = nil
 
             ticktime = Benchmark.realtime do
-              # detect MCL lag
-              if Thread.current[:last_tick] && (Time.current - Thread.current[:last_tick]) > (app.config["tick_rate"]*2)
-                diff = Time.current - Thread.current[:last_tick]
-                app.log.warn "[CORE] main loop is lagging (delayed for #{diff.to_f - app.config["tick_rate"]}s)"
-              end
-
-              # detect died minecraft server
-              if app.server.died?
-                app.log.fatal "[IPC] connection to minecraft server lost after tick ##{@tick - 1}, rebooting..."
-                raise Application::Reboot, "server connection lost after tick ##{@tick - 1}"
-              end
-
-              # detect reboot request
-              if $mcl_reboot
-                $mcl_reboot = false
-                raise Application::Reboot, "internal request"
-              end
-
-              # scrub async threads
-              if @tick % app.config["async_scrub_rate"] == 0
-                app.async.select!(&:alive?)
-              end
-
-              # scrub promises
-              if @tick % app.config["promise_scrub_rate"] == 0
-                app.promises.select!(&:alive?)
-              end
-
-              # player cache
-              if @tick % app.config["player_cache_save_rate"] == 0
-                app.delay { app.pman.clear_cache }
-              end
-
-              # garbage collection
-              if @tick % app.config["gc_rate"] == 0
-                GC.start
-              end
+              # pretick
+              detect_mcl_lag
+              detect_died_minecraft_server
+              detect_reboot_request
+              scrub_async_threads
+              scrub_promises
+              clear_player_cache
+              garbage_collect
 
               # process spool to events
               processtime = Benchmark.realtime do
-                begin
-                  # promises
-                  app.promises.each_with_index do |promise, i|
-                    begin
-                      promise.tick!
-                      app.promises.delete_at(i) unless promise.alive?
-                    rescue Exception
-                      app.handle_exception($!) do |ex|
-                        app.log.error "PromiseTickError on tick #{@tick}: (#{ex.class.name}) #{ex.message}"
-                      end
-                    end
-                  end
-
-                  # actual spool
-                  while !@spool.empty?
-                    val = @spool.pop(true) rescue nil
-                    break if val.nil?
-                    events += 1
-
-                    # handle stuff
-                    begin
-                      # parse event
-                      evd = parser.classify(val.chomp)
-                    rescue Exception
-                      app.handle_exception($!) do |ex|
-                        app.log.error "EventParseError on tick #{@tick}: (#{ex.class.name}) #{ex.message}"
-                      end
-                    end
-                  end
-                end
+                tick_promises
+                tick_spool { events += 1 }
               end
 
               # short tick handlers
-              shortticktime = Benchmark.realtime do
-                begin
-                  app.handlers.each do |handler|
-                    begin
-                      handler.tick!
-                    rescue Exception
-                      app.handle_exception($!) do |ex|
-                        app.log.error "ShortTickPerformError on tick #{@tick}: (#{ex.class.name}) #{ex.message}"
-                      end
-                    end
-                  end
-                end
-              end
+              shortticktime = Benchmark.realtime { tick_handlers }
 
               # delayed tasks
-              delayedtime = Benchmark.realtime do
-                while cb = app.delayed.shift
-                  begin
-                    cb.call
-                  rescue Exception
-                    app.handle_exception($!) do |ex|
-                      app.log.error "DelayedPerformError on tick #{@tick}: (#{ex.class.name}) #{ex.message}"
-                    end
-                  end
-                end
-              end
+              delayedtime = Benchmark.realtime { tick_delayed }
 
               # tick scheduler
-              schedulertime = Benchmark.realtime do
-                begin
-                  jobs = app.scheduler.tick!
-                rescue Exception
-                  app.handle_exception($!) do |ex|
-                    app.log.error "SchedulerPerformError on tick #{@tick}: (#{ex.class.name}) #{ex.message}"
-                  end
-                end
-              end
+              schedulertime = Benchmark.realtime { jobs = tick_scheduler }
             end
           ensure
             Thread.current[:last_tick] = Time.current
@@ -187,11 +220,8 @@ module Mcl
               diff = app.config["tick_rate"] - ticktime
               app.devlog "[T#{@tick}] #{events} events, #{jobs} jobs (RT: #{atime(ticktime)} P: #{atime(processtime)} ST: #{atime(shortticktime)} D: #{atime(delayedtime)} S: #{atime(schedulertime)} W: #{atime(diff)})"
 
-              # sleep and give the collector a explicit chance to do something
-              Thread.pass
-
-              # sleep rest of time if there is any left
-              sleep diff if diff > 0
+              Thread.pass # give other threads an explicit chance to do something
+              sleep diff if diff > 0 # sleep rest of tick rate if there is any left
             end
           end
         end
