94ae264f2fc5423b7f8260bcbc0af6448f4c1b83
Sven Pachnit <sven@bmonkeys.net>
2014-09-09 18:16:36 +0200

    Added cheats

--- vendor/handlers/_buildin/cheats.rb
+++ vendor/handlers/_buildin/cheats.rb
@@ -7,6 +7,16 @@ module Mcl
   # !balls [target]
   # !boat [target]
   # !minecart [target]
+  # !pick [target]
+  # !upick [target]
+  # !shovel [target]
+  # !ushovel [target]
+  # !axe [target]
+  # !uaxe [target]
+  # !speedegg [boost] [target]
+  # !slapbread [target]
+  # !slaystick [target]
+  # !lootbrick [boost] [target]
   class HMclCheats < Handler
     def setup
       register_l0(:guest)
@@ -15,6 +25,16 @@ module Mcl
       register_balls(:mod)
       register_boat(:mod)
       register_minecart(:mod)
+      register_pick(:mod)
+      register_upick(:mod)
+      register_shovel(:mod)
+      register_ushovel(:mod)
+      register_axe(:mod)
+      register_uaxe(:mod)
+      register_speedegg(:mod)
+      register_slapbread(:mod)
+      register_slaystick(:mod)
+      register_lootbrick(:mod)
     end
 
     def register_l0 acl_level
@@ -51,6 +71,96 @@ module Mcl
     def register_minecart acl_level
       register_command :minecart, desc: "summons a minecart above your or target's head", acl: acl_level do |player, args|
         $mcl.server.invoke "/execute #{args.first || player} ~ ~ ~ summon Minecart ~ ~2 ~"
+      end
+    end
+
+    def register_pick acl_level
+      register_command :pick, desc: "gives you a diamond pickaxe", acl: acl_level do |player, args|
+        $mcl.server.invoke %{/give #{args.first || player} minecraft:diamond_pickaxe 1 0 {display:{Name:"Cheated Pick"}}}
+      end
+    end
+
+    def register_upick acl_level
+      register_command :upick, desc: "gives you a highly enchanted diamond pick", acl: acl_level do |player, args|
+        $mcl.server.invoke %{
+          /give @a minecraft:diamond_pickaxe 1 0 {display:{Name:"OpPick"},Unbreakable:1,ench:[
+            {id:16,lvl:255},{id:20,lvl:255},{id:32,lvl:255},{id:33,lvl:255},{id:35,lvl:255}],
+            HideFlags:31,CanDestroy:["minecraft:stone","minecraft:grass","minecraft:dirt","minecraft:log","minecraft:planks"],Durability:-1}
+        }.gsub("\n", "").squeeze(" ")
+      end
+    end
+
+    def register_shovel acl_level
+      register_command :shovel, desc: "gives you a diamond shovel", acl: acl_level do |player, args|
+        $mcl.server.invoke %{/give #{args.first || player} minecraft:diamond_shovel 1 0 {display:{Name:"Cheated Shovel"}}}
+      end
+    end
+
+    def register_ushovel acl_level
+      register_command :ushovel, desc: "gives you a highly enchanted diamond shovel", acl: acl_level do |player, args|
+        $mcl.server.invoke %{
+          /give @a minecraft:diamond_shovel 1 0 {display:{Name:"OpShovel"},Unbreakable:1,ench:[
+            {id:16,lvl:255},{id:20,lvl:255},{id:32,lvl:255},{id:33,lvl:255},{id:35,lvl:255}],
+            HideFlags:31,CanDestroy:["minecraft:stone","minecraft:grass","minecraft:dirt","minecraft:log","minecraft:planks"],Durability:-1}
+        }.gsub("\n", "").squeeze(" ")
+      end
+    end
+
+    def register_axe acl_level
+      register_command :axe, desc: "gives you a diamond axe", acl: acl_level do |player, args|
+        $mcl.server.invoke %{/give #{args.first || player} minecraft:diamond_axe 1 0 {display:{Name:"Cheated Axe"}}}
+      end
+    end
+
+    def register_uaxe acl_level
+      register_command :uaxe, desc: "gives you a highly enchanted diamond axe", acl: acl_level do |player, args|
+        $mcl.server.invoke %{
+          /give @a minecraft:diamond_axe 1 0 {display:{Name:"OpAxe"},Unbreakable:1,ench:[
+            {id:16,lvl:255},{id:20,lvl:255},{id:32,lvl:255},{id:33,lvl:255},{id:35,lvl:255}],
+            HideFlags:31,CanDestroy:["minecraft:stone","minecraft:grass","minecraft:dirt","minecraft:log","minecraft:planks"],Durability:-1}
+        }.gsub("\n", "").squeeze(" ")
+      end
+    end
+
+    def register_speedegg acl_level
+      register_command :speedegg, desc: "gives you a speed egg which lets you run faster", acl: acl_level do |player, args|
+        boost = (args.first || 3).to_i / 10.0
+        $mcl.server.invoke %{
+          /give #{args.second || player} minecraft:egg 1 0 {display:{Name:"Speedegg #{(boost * 10).to_i}"},ench:[{id:0,lvl:255}],HideFlags:31,AttributeModifiers:[
+            {AttributeName:"generic.movementSpeed",Name:"generic.movementSpeed",Amount:#{boost},Operation:0,UUIDMost:98288,UUIDLeast:132143}
+          ]}
+        }.gsub("\n", "").squeeze(" ")
+      end
+    end
+
+    def register_slapbread acl_level
+      register_command :slapbread, desc: "gives you a slap bread", acl: acl_level do |player, args|
+        $mcl.server.invoke %{
+          /give #{args.first || player} minecraft:bread 1 0 {display:{Name:"Slapbread"},ench:[{id:19,lvl:255}],AttributeModifiers:[
+            {AttributeName:"generic.maxHealth",Name:"generic.maxHealth",Amount:40,Operation:0,UUIDMost:11947,UUIDLeast:137372},
+            {AttributeName:"generic.knockbackResistance",Name:"generic.knockbackResistance",Amount:1,Operation:0,UUIDMost:67558,UUIDLeast:116297},
+            {AttributeName:"generic.attackDamage",Name:"generic.attackDamage",Amount:0,Operation:0,UUIDMost:23120,UUIDLeast:188440}
+          ]}
+        }.gsub("\n", "").squeeze(" ")
+      end
+    end
+
+    def register_slaystick acl_level
+      register_command :slaystick, desc: "gives you a lethal stick", acl: acl_level do |player, args|
+        $mcl.server.invoke %{
+          /give #{args.first || player} minecraft:stick 1 0 {display:{Name:"Slaystick"},ench:[{id:20,lvl:255}],AttributeModifiers:[
+            {AttributeName:"generic.maxHealth",Name:"generic.maxHealth",Amount:40,Operation:0,UUIDMost:11947,UUIDLeast:137372},
+            {AttributeName:"generic.knockbackResistance",Name:"generic.knockbackResistance",Amount:1,Operation:0,UUIDMost:67558,UUIDLeast:116297},
+            {AttributeName:"generic.attackDamage",Name:"generic.attackDamage",Amount:255,Operation:0,UUIDMost:23120,UUIDLeast:188440}
+          ]}
+        }.gsub("\n", "").squeeze(" ")
+      end
+    end
+
+    def register_lootbrick acl_level
+      register_command :lootbrick, desc: "gives you a loot brick", acl: acl_level do |player, args|
+        boost = args.first || 255
+        $mcl.server.invoke %{/give #{args.second || player} minecraft:brick 1 0 {HideFlags:31,display:{Name:"Lootbrick"},ench:[{id:21,lvl:#{boost}}]}}
       end
     end
   end
