739beb18b4c2ecbbd70d6930f98f3d95df70b75d
Sven Pachnit <sven@bmonkeys.net>
2014-10-26 04:51:50 +0100

    Create a backup of the sqlite database before applying new migrations.

--- lib/mcl/application/db_schema.rb
+++ lib/mcl/application/db_schema.rb
@@ -3,7 +3,20 @@ module Mcl
     module DbSchema
       def define_database_schema
         ActiveRecord::Migration.verbose = true
-        ActiveRecord::Migrator.migrate Dir["#{ROOT}/**/migrations"], ENV['VERSION'].try(:to_i) ? ENV['VERSION'].to_i : nil
+        migrator = ActiveRecord::Migrator.open(Dir["#{ROOT}/**/migrations"])
+        missing = migrator.pending_migrations
+        log.debug "[Migrator] found #{migrator.migrations.count} migrations, #{missing.count} yet to migrate."
+
+        if missing.any?
+          # create backup of sqlite DB
+          if config["database"]["adapter"] == "sqlite3"
+            log.debug "[Migrator] creating backup of sqlite DB"
+            FileUtils.cp(@config["database"]["database"], "#{@config["database"]["database"]}.#{Time.current.strftime("%Y-%m-%d_%H-%M-%S")}.backup")
+          end
+
+          log.debug "[Migrator] migrating #{missing.count} migrations..."
+          migrator.migrate
+        end
       end
 
       # def define_database_schema
