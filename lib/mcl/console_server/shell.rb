a7854895a83f1e8ae4521da0f61f4f2ecb982967
Sven Pachnit <sven@bmonkeys.net>
2014-10-28 23:21:45 +0100

    Shell stuff

--- lib/mcl/console_server/shell.rb
+++ lib/mcl/console_server/shell.rb
@@ -1,6 +1,42 @@
 module Mcl
   class ConsoleServer
     class Shell
+      # exit / quit        get out of here
+      # help               show a list of commands
+      # !<mcl command>     invoke a MCL command
+      # /<mc command>      invoke a command on the server console
+      # .<msg>             shortcut for /say
+      #
+      # # all of these work for `mlog` as well which is for MCL output
+      # log                same as `log 20`
+      # log <N>            shows last N lines of output (shortcut for -n)
+      # log <on/off>       turn live logging on or off (when turning on you can combine -f -t -g)
+      # ------
+      # log -h             shows this help
+      # log -n N           shows last N lines of output (if available)
+      # log -g regex       grep/search backlog with regular expression (can be combined with -n which is 1000 by default)
+      # log -a regex       add a regular expression filter for events, matches will NOT be shown
+      # log -f             apply filters (to use like `log 100 -f`)
+      # log -l             list all filters
+      # log -d <filter>    delete filter (you can use filter or the index from -l or * to remove all)
+      #
+      # # these only work for log (not for mlog)
+      # log -t <type>      only show of type (e.g.: log 100 -t join,chat)
+      #                       chat     chat messages
+      #                       join     player connects
+      #                       leave    player disconnects
+      #                       state    join + leave
+      #
+      # # sessions
+      # sessions           shows a list of open console sessions
+      # msg <sid> <msg>    send a message to given session
+      # broadcast <msg>    send a message to all sessions
+      # nick <name>        name your session
+      #
+      # # protocol
+      # env                shows your current environment
+      # set_env <data>     apply settings from client (filters, etc.)
+      # set_client <data>  set session client id
       attr_reader :session, :server, :app
 
       def initialize(session)
@@ -32,8 +68,13 @@ module Mcl
         if str.strip == "exit"
           session.terminate("client quit")
         else
-          oputs ": #{str}"
-          session.cputs ": #{str}"
+          critical do
+            if str.strip == "moep"
+              15.times {|n| `say #{n}` ; sleep 0.5 ; break if session.halting }
+            end
+            oputs ": #{str}"
+            session.cputs ": #{str}"
+          end
         end
       end
 
--- lib/mcld.rb
+++ lib/mcld.rb
@@ -5,37 +5,44 @@ require "active_support"
 require "active_support/core_ext"
 PROJECT_ROOT = Pathname.new File.expand_path("../..", __FILE__)
 
-# Require elevated privileges on windows
-require "#{PROJECT_ROOT}/lib/mcl"
-if Mcl.windows?
-  def running_in_admin_mode?
-    system("net session >nul 2>&1")
-  end
-
-  if !running_in_admin_mode?
-    puts "========================================================"
-    puts "= MCL must be started from an elevated command prompt! ="
-    print "========================================================"
-    exit 1
-  end
-end
-
 # Instance
 MCL_INSTANCE = ENV["MCL_INSTANCE"].presence || ENV["MCLI"].presence || "default"
 
-# Ensure directories
-FileUtils.mkdir_p("#{PROJECT_ROOT}/tmp")
-FileUtils.mkdir_p("#{PROJECT_ROOT}/log")
+if ARGV[0] == "console" || ARGV[0] == "c"
+  STDOUT.sync = true
+  ["mcl", "mcl/core"].each{|l| require "#{PROJECT_ROOT}/lib/#{l}" }
+  raise "moep"
+  Mcl::ConsoleClient.new.dispatch(MCL_INSTANCE)
+else
+  # Require elevated privileges on windows
+  require "#{PROJECT_ROOT}/lib/mcl"
+  if Mcl.windows?
+    def running_in_admin_mode?
+      system("net session >nul 2>&1")
+    end
 
-# Run daemon
-Daemons.run("#{PROJECT_ROOT}/lib/mcl/daemon.rb",
-  app_name: "mcld_#{MCL_INSTANCE}",
-  dir_mode: :normal,               # use absolute path
-  dir: "#{PROJECT_ROOT}/tmp",      # pid directory
-  log_output: true,                # log application output
-  log_dir: "#{PROJECT_ROOT}/log",  # log directory
-  backtrace: true,                 # log backtrace on crash
-  multiple: false,                 # allow only 1 instance
-  monitor: true,                   # restart app on crash
-  force_kill_waittime: 90          # wait before killing
-)
+    if !running_in_admin_mode?
+      puts "========================================================"
+      puts "= MCL must be started from an elevated command prompt! ="
+      print "========================================================"
+      exit 1
+    end
+  end
+
+  # Ensure directories
+  FileUtils.mkdir_p("#{PROJECT_ROOT}/tmp")
+  FileUtils.mkdir_p("#{PROJECT_ROOT}/log")
+
+  # Run daemon
+  Daemons.run("#{PROJECT_ROOT}/lib/mcl/daemon.rb",
+    app_name: "mcld_#{MCL_INSTANCE}",
+    dir_mode: :normal,               # use absolute path
+    dir: "#{PROJECT_ROOT}/tmp",      # pid directory
+    log_output: true,                # log application output
+    log_dir: "#{PROJECT_ROOT}/log",  # log directory
+    backtrace: true,                 # log backtrace on crash
+    multiple: false,                 # allow only 1 instance
+    monitor: true,                   # restart app on crash
+    force_kill_waittime: 90          # wait before killing
+  )
+end
