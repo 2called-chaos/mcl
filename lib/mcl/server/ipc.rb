4a86af45117f9c2f5513b63c6f7255ea1d20e712
Sven Pachnit <sven@bmonkeys.net>
2014-10-29 11:20:51 +0100

    Start subprocess in own process group to prevent the shell of sending INT signals to it (10 bucks on "this will fail on windows")

--- lib/mcl/server/ipc.rb
+++ lib/mcl/server/ipc.rb
@@ -79,7 +79,7 @@ module Mcl
           app.log.info "[IPC] starting minecraft server..."
           # @_ipc_stdin, @_ipc_stdouterr, @_ipc_thread = Open3.popen2e(%{cd "#{app.server.root}" && exec #{app.config["launch_cmd"]}})
           Dir.chdir("#{app.server.root}")
-          @_ipc_stdin, @_ipc_stdouterr, @_ipc_thread = Open3.popen2e(%{#{app.config["launch_cmd"]}})
+          @_ipc_stdin, @_ipc_stdouterr, @_ipc_thread = Open3.popen2e(%{#{app.config["launch_cmd"]}}, pgroup: true)
           app.log.debug "[IPC] server running with pid #{@_ipc_thread.pid}"
         end
       end
