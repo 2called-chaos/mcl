236283cbf8c08c75f94d1fa2a6a1e03d7dffa6fe
Sven Pachnit <sven@bmonkeys.net>
2018-08-01 15:34:36 +0200

    Fix position detection being one off when negative

--- lib/mcl/handlers/misc.rb
+++ lib/mcl/handlers/misc.rb
@@ -9,7 +9,11 @@ module Mcl
 
       # player position detection (1.13)
       register_parser(/\ATeleported ([^\s]+) to ([\d\.,\-]+),(?:\s)?([\d\.,\-]+),(?:\s)?([\d\.,\-]+?)(?:\.)?\z/i) do |res, r|
-        pmemo(r[1])[:detected_pos] = [r[2].to_i, r[3].to_i, r[4].to_i - 1]
+        x, y, z = r[2].to_f, r[3].to_f, r[4].to_f
+        x -= 1 if x < 0
+        y -= 1 if y < 0
+        z -= 1 if z < 0
+        pmemo(r[1])[:detected_pos] = [x.to_i, y.to_i, z.to_i]
       end
     end
   end
